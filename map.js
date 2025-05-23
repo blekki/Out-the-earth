242eb24a948080f2b77eb792bab0f4b36225c2a3
Orest <orest.chumak@gmail.com>
2025-05-23 11:11:07 +0300

    Change camera rotation system

--- map.js
+++ map.js
@@ -3,18 +3,35 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/js
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+// PerspectiveCamera was 75
 let mouseIsHold = false;
-let startX = 0; let viewX = 0;
-let startY = 0; let viewY = 0;
-// let mouseX = window.innerWidth / 2;
-// let mouseY = window.innerHeight / 2;
-// let controls; // move camera around the scene
+let startX = 0; let deltaX = 0;
+let startY = 0; let deltaY = 0;
+
+// <> Queternions <>
+// var matrix4 = new THREE.Matrix4();
+// const quaternion = new THREE.Quaternion();
+
+var camera_position = new THREE.Vector3(0, 0, 0);
+var camera_look_at = new THREE.Vector3(0, 0, -1);
+var camera_up = new THREE.Vector3(0, 1, 0);
+var camera_direction = new THREE.Vector3(0, 0, -1);
+var camera_position_delta = new THREE.Vector3(0, 0, 0);
+
+var camera_pitch   = 0;//Math.PI / 360.0;
+var camera_heading = 0;//Math.PI / 360.0;
+
+var pitch_quat = new THREE.Quaternion(0, 0, 0, 1);
+var heading_quat = new THREE.Quaternion(0, 0, 0, 1);
+var temp = new THREE.Quaternion(0, 0, 0, 1);
+
+
+
 
 let object; // 3D object on a global variable
-let objSource = 'space-sphere-ico.glb'; // object to render
-
+// let objSource = 'space-sphere-ico.glb'; // object to render
+let objSource = 'star-map-test.glb';
 // Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 loader.load(
@@ -31,31 +48,29 @@ loader.load(
         console.error(error);
     }
 );
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 0;
+
+camera.rotation.x = 0;
+camera.rotation.y = 0;
+camera.rotation.z = 0;
 
 // instantiate a new renderer and set its size
 const renderer = new THREE.WebGLRenderer({ alpha: true }); // true allows transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-//Add the renderer to the DOM
+// add the renderer to the DOM
 document.getElementById("container3D").appendChild(renderer.domElement);
 
-//Set how far the camera will be from the 3D model
-// camera.position.z = objSource === "dino" ? 25 : 500;
-camera.position.z = objSource = 0;
+// add lights to the scene
+// const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+// topLight.position.set(500, 500, 500) //top-left-position
+// topLight.castShadow = true;
+// scene.add(topLight);
 
-//Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-topLight.position.set(500, 500, 500) //top-left-position
-topLight.castShadow = true;
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, objSource === "dino" ? 5 : 1);
-scene.add(ambientLight);
-
-//This adds controls to the camera, so we can rotate / zoom it with the mouse
-// if (objSource) {
-    // controls = new OrbitControls(camera, renderer.domElement);
-// }
+// const ambientLight = new THREE.AmbientLight(0x333333, objSource === "dino" ? 5 : 1);
+// scene.add(ambientLight);
 
 // render scene
 function animate() {
@@ -64,15 +79,41 @@ function animate() {
   // <-- here we could add some code to update scene, adding some automatic movement
 
   if (mouseIsHold) {
-    // object.rotation.y = mouseX / (window.innerWidth * 3.0);
-    // object.rotation.x = mouseY * 1.7 / (window.innerHeight * 3.0);
-    camera.rotation.y = viewX;
-    camera.rotation.x = viewY;
-    //  todo: rotation with quaternions
+    // camera.rotation.x = deltaY;
+    // camera.rotation.y = deltaX;
+    // camera.rotation.z = deltaX;
+    // camera.rotation.y = deltaX;// * Math.cos(deltaY);
+    // camera.rotation.z = deltaX;// * Math.sin(deltaY);
+
+
+    // <> quet <>
+    camera_direction.subVectors(camera_look_at, camera_position).normalize();
+    var axis = new THREE.Vector3(0, 0, 0);
+    axis.crossVectors(camera_direction, camera_up);
+
+
+    pitch_quat.setFromAxisAngle(axis, camera_pitch);
+    heading_quat.setFromAxisAngle(camera_up, camera_heading);
+    temp.multiplyQuaternions(pitch_quat, heading_quat);
+
+    camera_direction.applyQuaternion(temp);
+    
+    // camera.rotation.x = camera_direction.x;
+    // camera.rotation.y = camera_direction.y;
+    // camera.rotation.z = camera_direction.z;
+
+    // camera_position.add(camera_position_delta);
+    camera_look_at.addVectors(camera_position, camera_direction);
+    // camera.position.copy(camera_position);
+    camera.up.copy(camera_up);
+    camera.lookAt(camera_look_at);
+
+    camera_pitch   = 0;
+    camera_heading = 0;
+
+    // console.log(camera.rotation.x, camera.rotation.y, camera.rotation.z);
+    // camera_position_delta.multiplyScalar(.8);
   }
-  // make the camera move
-  // object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-  // object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
   renderer.render(scene, camera);
 }
 
@@ -84,22 +125,30 @@ window.addEventListener("resize", function () {
 });
 
 
+
+
 // rotation pack
 document.onmousemove = (e) => {
   if (mouseIsHold) {
-    viewX += (e.clientX - startX) / window.innerWidth;
-    viewY += (e.clientY - startY) / window.innerHeight;
+    // deltaX += (e.clientX - startX) / window.innerWidth;
+    // deltaY += (e.clientY - startY) / window.innerHeight;
+    deltaX = (e.clientX - startX) / window.innerWidth;
+    deltaY = (e.clientY - startY) / window.innerHeight;
     startX = e.clientX;
     startY = e.clientY;
+
+    // camera_pitch   = (Math.abs(deltaY) > 0.01) ? deltaY : 0.0;
+    // camera_heading = (Math.abs(deltaX) > 0.01) ? deltaX : 0.0;
+    camera_pitch   = deltaY;
+    camera_heading = deltaX;
+    console.log(temp.x, " : ", temp.y);
   }
 }
-
 document.onmousedown = (e) => {
   mouseIsHold = true;
   startX = e.clientX;
   startY = e.clientY;
 }
-
 document.onmouseup = () => {
   mouseIsHold = false;
 }
